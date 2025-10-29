226a4f3982f9a9d830ba9fa07d6b3b30354f7168
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-29 11:10:22 +0100

    création d'une classe operators + reponses question 6.1

---
+++ src/fr/ex/operation/Operators.java
@@ -0,0 +1,32 @@
+package fr.ex.operation;
+
+public class Operators {
+	
+	// Question 6.1
+	public static int add(int x, int y) {
+		return x + y;
+	}
+	
+	public static int sub(int x, int y) {
+		return x - y;
+	}
+	
+	public static int multiply(int x, int y) {
+		return x * y;
+	}
+	
+	public static int divide(int x, int y) {
+		if (x != 0 && y !=0) {
+			return x / y;
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Result of 5 + 2: " + add(5, 2));
+		System.out.println("Result of 5 - 2: " + sub(5, 2));
+		System.out.println("Result of 3 / 0: " + divide(3, 0));
+	}
+
+}
