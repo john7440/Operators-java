fd8619173df57f76e2bd51fe2cdf69790b8a7a3a
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-31 09:07:05 +0100

    ajout de modification a divide + ajout de commentaires

--- src/fr/ex/operation/Operators.java
+++ src/fr/ex/operation/Operators.java
@@ -3,31 +3,36 @@ package fr.ex.operation;
 public class Operators {
 	
 	// Question 6.1
+	
+	// Method to add 2 integers
 	public static int add(int x, int y) {
 		return x + y;
 	}
 	
+	// Method to subtract two integers
 	public static int sub(int x, int y) {
 		return x - y;
 	}
 	
+	// Method to multiply 2 integers
 	public static int multiply(int x, int y) {
 		return x * y;
 	}
 	
+	// Method to divide 2 integers, it returns the first integer if we try to divide by 0
 	public static int divide(int x, int y) {
-		if (x != 0 && y !=0) {
+		if (y != 0) {
 			return x / y;
 		}
-		return 0;
+		return x;
 	}
 
 	public static void main(String[] args) {
 		
-		
-		// Tests
+		// Tests for each method
 		System.out.println("Result of 5 + 2: " + add(5, 2));
 		System.out.println("Result of 5 - 2: " + sub(5, 2));
+		System.out.println("Result of 5 * 2: " + multiply(5, 2));
 		System.out.println("Result of 3 / 0: " + divide(3, 0));
 	}
 
