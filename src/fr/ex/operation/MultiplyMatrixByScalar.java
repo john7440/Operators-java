b4679598343125f61061348ac3fd527a8c57dfa0
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-31 09:37:32 +0100

    ajout de comentaire + docstring

--- src/fr/ex/operation/MultiplyMatrixByScalar.java
+++ src/fr/ex/operation/MultiplyMatrixByScalar.java
@@ -3,14 +3,22 @@ package fr.ex.operation;
 // Question 6.6
 public class MultiplyMatrixByScalar {
 	
+	/**
+     * Multiplies each element of a matrix by a scalar value.
+     *
+     * @param matrix the input matrix
+     * @param scalar the scalar value to multiply each element by
+     * @return a new matrix containing the scaled values
+     */
 	public static int[][] multiplyByScalar(int[][] matrix, int scalar){
 		
-		//This method can multiply a matrix by a scalar
 		int rows = matrix.length;
 		int cols = matrix[0].length;
 		
 		int[][] result = new int[rows][cols];
 		
+		
+		// We multiply each element by the scalar
 		for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 result[i][j] = matrix[i][j] * scalar;
@@ -21,10 +29,13 @@ public class MultiplyMatrixByScalar {
 	}
 
 	public static void main(String[] args) {
-		// This is our main method, it initialize a matrix and a scalar,
-		// then we call the multiplyByScalar method and print the result
-		// with the printMatrix method from our class SumAndSubMAtrix:
-		
+		/**
+         * Main method:
+         * - Initializes a sample matrix and a scalar value
+         * - Calls multiplyByScalar to perform the operation
+         * - Prints the resulting matrix using the printMatrix method from SumAndSubMatrix
+         */
+        
 		int[][] matrix3 = {
 				{0, 2},
 				{1, 2},
@@ -32,8 +43,11 @@ public class MultiplyMatrixByScalar {
 				
 		};
 
+		// Multiply the matrix by the scalar
 		int scalar = 2;
 		int[][] matrix3Scalar = multiplyByScalar(matrix3, scalar);
+		
+		// Print the result using the method from the SumAndSubMatrix class
 		SumAndSubMatrix.printMatrix(matrix3Scalar);
 
 	}
